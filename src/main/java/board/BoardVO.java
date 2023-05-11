49a08c677af5d66c686a6a19c20c507929440d30
beakhh <baik5634@naver.com>
2023-05-11 17:17:43 +0900

    2023-05-11_17:17

--- src/main/java/board/BoardVO.java
+++ src/main/java/board/BoardVO.java
@@ -24,122 +24,169 @@ public class BoardVO {
 	private String preTitle;
 	private String nextTitle;
 	
+	private int replyCount; // 댓글의 개수
+
 	public int getIdx() {
 		return idx;
 	}
+
 	public void setIdx(int idx) {
 		this.idx = idx;
 	}
+
 	public String getMid() {
 		return mid;
 	}
+
 	public void setMid(String mid) {
 		this.mid = mid;
 	}
+
 	public String getNickName() {
 		return nickName;
 	}
+
 	public void setNickName(String nickName) {
 		this.nickName = nickName;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getHomePage() {
 		return homePage;
 	}
+
 	public void setHomePage(String homePage) {
 		this.homePage = homePage;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	public int getReadNum() {
 		return readNum;
 	}
+
 	public void setReadNum(int readNum) {
 		this.readNum = readNum;
 	}
+
 	public String getHostIp() {
 		return hostIp;
 	}
+
 	public void setHostIp(String hostIp) {
 		this.hostIp = hostIp;
 	}
+
 	public String getOpenSw() {
 		return openSw;
 	}
+
 	public void setOpenSw(String openSw) {
 		this.openSw = openSw;
 	}
+
 	public String getwDate() {
 		return wDate;
 	}
+
 	public void setwDate(String wDate) {
 		this.wDate = wDate;
 	}
+
 	public int getGood() {
 		return good;
 	}
+
 	public void setGood(int good) {
 		this.good = good;
 	}
+
 	public int getDay_diff() {
 		return day_diff;
 	}
+
 	public void setDay_diff(int day_diff) {
 		this.day_diff = day_diff;
 	}
+
 	public int getHour_diff() {
 		return hour_diff;
 	}
+
 	public void setHour_diff(int hour_diff) {
 		this.hour_diff = hour_diff;
 	}
+
 	public int getPreIdx() {
 		return preIdx;
 	}
+
 	public void setPreIdx(int preIdx) {
 		this.preIdx = preIdx;
 	}
+
 	public int getNextIdx() {
 		return nextIdx;
 	}
+
 	public void setNextIdx(int nextIdx) {
 		this.nextIdx = nextIdx;
 	}
+
 	public String getPreTitle() {
 		return preTitle;
 	}
+
 	public void setPreTitle(String preTitle) {
 		this.preTitle = preTitle;
 	}
+
 	public String getNextTitle() {
 		return nextTitle;
 	}
+
 	public void setNextTitle(String nextTitle) {
 		this.nextTitle = nextTitle;
 	}
+
+	public int getReplyCount() {
+		return replyCount;
+	}
+
+	public void setReplyCount(int replyCnt) {
+		this.replyCount = replyCnt;
+	}
+
 	@Override
 	public String toString() {
 		return "BoardVO [idx=" + idx + ", mid=" + mid + ", nickName=" + nickName + ", title=" + title + ", email=" + email
 				+ ", homePage=" + homePage + ", content=" + content + ", readNum=" + readNum + ", hostIp=" + hostIp
 				+ ", openSw=" + openSw + ", wDate=" + wDate + ", good=" + good + ", day_diff=" + day_diff + ", hour_diff="
 				+ hour_diff + ", preIdx=" + preIdx + ", nextIdx=" + nextIdx + ", preTitle=" + preTitle + ", nextTitle="
-				+ nextTitle + "]";
+				+ nextTitle + ", replyCnt=" + replyCount + "]";
 	}
+	
 	
 	
 }
